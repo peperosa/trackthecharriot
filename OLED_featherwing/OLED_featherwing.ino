7189f33b9a2b07cefc330cb9c738ec9f7b72f314
peperosa <53124849+peperosa@users.noreply.github.com>
2019-07-26 13:22:49 -0700

    initial commit
    Includes:
    - Feather 9x_TX/RX to make the TX board feather sending helloworld and the RX to send a reply (needs the RadioHead library)
    - GPS_Hardware_SerialEchoTest shows on the serial port the GPS received coordinates, when the GPS is FIX the FIX LED of the board blinks every 15s, if not in FIX status the LED blinks every second (no need for library, I did include though the TinyGPS library that I am gonna use for coordinates handling)
    - OLED_featherwing is supposed to shows on the display a message on the startup and show A/B/C when pressing the buttons, currently not working probably because of wobbly connections (needs the Adafruit_GFX_Library and Adafruit_SSD1306 library)

---
+++ OLED_featherwing/OLED_featherwing.ino
@@ -0,0 +1,64 @@
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <SPI.h>
+#include <Wire.h>
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+
+// OLED FeatherWing buttons map to different pins depending on board:
+// 32u4, M0, M4, nrf52840 and 328p
+#define BUTTON_A  9
+#define BUTTON_B  6
+#define BUTTON_C  5
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("OLED FeatherWing test");
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+
+  Serial.println("OLED begun");
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  Serial.println("IO test");
+
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Connecting to SSID\n'adafruit':");
+  display.print("connected!");
+  display.println("IP: 10.0.1.23");
+  display.println("Sending val #0");
+  display.setCursor(0,0);
+  display.display(); // actually display all of the above
+}
+
+void loop() {
+  if(!digitalRead(BUTTON_A)) display.print("A");
+  if(!digitalRead(BUTTON_B)) display.print("B");
+  if(!digitalRead(BUTTON_C)) display.print("C");
+  delay(10);
+  yield();
+  display.display();
+}
